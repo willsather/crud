0d655596f51e468ae9361439ad3ab9bc41aee83b
Will Sather <satherw@vmware.com>
2024-08-19 17:38:19 -0500

    fix: rename controller

--- springboot/src/test/java/com/satherw/crud/PersonControllerTests.java
+++ springboot/src/test/java/com/satherw/crud/PersonControllerTests.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Testcontainers
 @Transactional
 @AutoConfigureMockMvc
-class CrudControllerTests {
+class PersonControllerTests {
 
 	@Container
 	@ServiceConnection
